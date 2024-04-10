b96f89e2a937b7984c5bbf736975ef18337bc707
Angelina <angelforworkpay@gmail.com>
2024-04-10 12:57:16 +0300

    feat:add app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const buttonColor = Color(0xff6A62B7);
+  static const white = Color(0xffF5F5F5);
+  static const black = Color(0xff141414);
+  static const gray = Color(0xffA0A0A0);
+  static const inputGray = Color(0xff636363);
+  static const inactive = Color(0xffB4B0DA);
+}
